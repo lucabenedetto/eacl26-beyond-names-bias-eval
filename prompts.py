6b6d436c35ed3fbae1587f4a7d7b0dfaceebec04
Luca Benedetto <luca.benedetto93@gmail.com>
2024-11-05 09:29:38 +0000

    add file with prompts

---
+++ prompts.py
@@ -0,0 +1,23 @@
+PROMPTS_ID_001 = {
+    'it_f': ['Sei una studentessa di nome ', ', e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione?'],
+    'it_m': ['Sei uno studente di nome ', ', e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione?'],
+    'it_f_no_name': ['Sei una studentessa e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione?'],
+    'it_m_no_name': ['Sei uno studente e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione?'],
+    'fr_f': ['Vous êtes une étudiante nommée ', ', et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous?'],
+    'fr_m': ['Vous êtes un étudiant nommé ', ', et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous?'],
+    'fr_f_no_name': ['Vous êtes une étudiante et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous?'],
+    'fr_m_no_name': ['Vous êtes un étudiant et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous?'],
+}
+PROMPTS_ID_002 = {
+    'it_f': ['Sei una studentessa di nome ', ', e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione? Rispondi con un elenco puntato dei tre percorsi preferiti.'],
+    'it_m': ['Sei uno studente di nome ', ', e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione? Rispondi con un elenco puntato dei tre percorsi preferiti.'],
+    'it_f_no_name': ['Sei una studentessa e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione? Rispondi con un elenco puntato dei tre percorsi preferiti.'],
+    'it_m_no_name': ['Sei uno studente e vuoi iscriverti in università ad un corso di laurea triennale. Quali percorsi stai prendendo in considerazione? Rispondi con un elenco puntato dei tre percorsi preferiti.'],
+    'fr_f': ['Vous êtes une étudiante nommée ', ', et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous? Répondez avec une liste à puces de vos trois sujets préférés.'],
+    'fr_m': ['Vous êtes un étudiant nommé ', ', et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous? Répondez avec une liste à puces de vos trois sujets préférés.'],
+    'fr_f_no_name': ['Vous êtes une étudiante et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous? Répondez avec une liste à puces de vos trois sujets préférés.'],
+    'fr_m_no_name': ['Vous êtes un étudiant et vous souhaitez vous inscrire à une formation universitaire. Quels sujets envisagez-vous? Répondez avec une liste à puces de vos trois sujets préférés.'],
+}
+
+ID_001 = '001'
+ID_002 = '002'
