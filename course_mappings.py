6e9acf2ac1888a55e306e4313634ec7a916bc055
Luca Benedetto <luca.benedetto93@gmail.com>
2024-12-13 10:19:54 +0000

    add first version of the file with the mapping between different wordings of the same courses

---
+++ course_mappings.py
@@ -0,0 +1,57 @@
+COURSE_MAPPINGS_IT = {
+    # --> 'psicologia'
+    'psychology': 'psicologia',
+    'psychologia': 'psicologia',
+    'scienze psicologiche': 'psicologia',
+    'psicologia clinica': 'psicologia',
+    # --> 'scienze politiche e relazioni internazionali'
+    'scienze politiche e delle relazioni internazionali': 'scienze politiche e relazioni internazionali',
+    'scienze politiche': 'scienze politiche e relazioni internazionali',
+    'relazioni internazionali': 'scienze politiche e relazioni internazionali',
+    'scienze internazionali e diplomatiche': 'scienze politiche e relazioni internazionali',
+    # --> 'lettere moderne'
+    'lingue e culture moderne': 'lettere moderne',
+    # --> 'lettere e filosofia'
+    'letteratura e filosofia': 'lettere e filosofia',
+    # --> 'letterature straniere'
+    'lingue e letterature straniere': 'letterature straniere',
+    'letterature e lingue straniere': 'letterature straniere',
+    'letteratura, lingue e culture straniere': 'letterature straniere',
+    'lingue e culture straniere': 'letterature straniere',
+    # --> 'biologia'
+    'biologia o scienze biologiche': 'biologia',
+    'scienze biologiche': 'biologia',
+    # --> 'economia e management'
+    'economia e finanza': 'economia e management',
+    'economia e gestione aziendale': 'economia e management',
+    'economia aziendale': 'economia e management',
+    'economia e commercio': 'economia e management',
+    'economia': 'economia e management',
+    'scienze economiche': 'economia e management',
+    'scienze economiche e bancarie': 'economia e management',
+    'economia e gestione delle imprese': 'economia e management',
+    'management e consulenza aziendale': 'economia e management',
+    "management e comunicazione d'impresa": 'economia e management',
+    'management internazionale': 'economia e management',
+    # ? 'ingegneria gestionale': 'economia e management',
+    # --> 'design'
+    'design e arti visive': 'design',
+    'arti e design': 'design',
+    'arte e design': 'design',
+    'design della comunicazione': 'design',  # design della comunicazione as different category?? ## TODO: could also be "comunicazione e marketing"
+    'design e comunicazione': 'design',
+    'design e comunicazione visiva': 'design',
+    "design del prodotto e dell'innovazione": 'design',  # or 'design del prodotto' as different category??
+    "design del prodotto e dell'evento": 'design',
+    "design del prodotto": 'design',
+    #
+    'medicina': 'medicina e chirurgia',
+    #
+    'comunicazione e media digitali': 'comunicazione e media',
+    #
+    'comunicazione e marketing digitale': 'comunicazione e marketing',  # together with the previous one?
+    #
+    # ?? 'informatica: 'ingegneria informatica'
+    'data science e intelligenza artificiale': 'data science e artificial intelligence',
+    'data science': 'data science e artificial intelligence',
+}
