0fd8fd2449e46de00b928551ce38d3be64cfbe11
Luca Benedetto <luca.benedetto93@gmail.com>
2024-12-13 10:41:49 +0000

    add new entries to the mapping dict.

--- course_mappings.py
+++ course_mappings.py
@@ -4,28 +4,51 @@ COURSE_MAPPINGS_IT = {
     'psychologia': 'psicologia',
     'scienze psicologiche': 'psicologia',
     'psicologia clinica': 'psicologia',
+
     # --> 'scienze politiche e relazioni internazionali'
     'scienze politiche e delle relazioni internazionali': 'scienze politiche e relazioni internazionali',
     'scienze politiche': 'scienze politiche e relazioni internazionali',
     'relazioni internazionali': 'scienze politiche e relazioni internazionali',
     'scienze internazionali e diplomatiche': 'scienze politiche e relazioni internazionali',
+
+    # --> lettere (including 'filosofia')
     # --> 'lettere moderne'
-    'lingue e culture moderne': 'lettere moderne',
+    'lingue e culture moderne': 'lettere',
+    'lingue e letterature moderne': 'lettere',
+    'lettere moderne': 'lettere',
+    #
+    'letteratura': 'lettere',
+    'letteratura italiana': 'lettere',
+    'letteratura comparata': 'lettere',
+    'letterature comparate': 'lettere',
+    'lingua e letteratura italiana': 'lettere',
     # --> 'lettere e filosofia'
-    'letteratura e filosofia': 'lettere e filosofia',
+    'letteratura e filosofia': 'lettere',
     # --> 'letterature straniere'
-    'lingue e letterature straniere': 'letterature straniere',
-    'letterature e lingue straniere': 'letterature straniere',
-    'letteratura, lingue e culture straniere': 'letterature straniere',
-    'lingue e culture straniere': 'letterature straniere',
+    'lingue e letterature straniere': 'lettere',
+    'lingua e letterature straniere': 'lettere',
+    'letterature e lingue straniere': 'lettere',
+    'letteratura, lingue e culture straniere': 'lettere',
+    'lingue e letterature straniere moderne': 'lettere',
+    'lingue e culture straniere': 'lettere',
+    'lingua e letteratura inglese': 'lettere',
+    # --> linguistica
+    'linguistica': 'lettere',
+    'linguistica applicata': 'lettere',
+    'linguistica e letteratura italiana': 'lettere',
+    'linguistica e letterature comparate': 'lettere',
+    'linguistica italiana': 'lettere',
+
     # --> 'biologia'
     'biologia o scienze biologiche': 'biologia',
     'scienze biologiche': 'biologia',
+
     # --> 'economia e management'
     'economia e finanza': 'economia e management',
     'economia e gestione aziendale': 'economia e management',
     'economia aziendale': 'economia e management',
     'economia e commercio': 'economia e management',
+    'economia e commercio internazionale': 'economia e management',
     'economia': 'economia e management',
     'scienze economiche': 'economia e management',
     'scienze economiche e bancarie': 'economia e management',
@@ -34,6 +57,7 @@ COURSE_MAPPINGS_IT = {
     "management e comunicazione d'impresa": 'economia e management',
     'management internazionale': 'economia e management',
     # ? 'ingegneria gestionale': 'economia e management',
+
     # --> 'design'
     'design e arti visive': 'design',
     'arti e design': 'design',
@@ -44,14 +68,25 @@ COURSE_MAPPINGS_IT = {
     "design del prodotto e dell'innovazione": 'design',  # or 'design del prodotto' as different category??
     "design del prodotto e dell'evento": 'design',
     "design del prodotto": 'design',
+
     #
     'medicina': 'medicina e chirurgia',
+
     #
     'comunicazione e media digitali': 'comunicazione e media',
+    'comunicazione': 'comunicazione e media',
+    'comunicazione e giornalismo': 'comunicazione e media',
+    'comunicazione e tecniche giornalistiche': 'comunicazione e media',
+    'comunicazione e relazioni pubbliche': 'comunicazione e media', # ??
+    'scienze della comunicazione': 'comunicazione e media', # ??
     #
     'comunicazione e marketing digitale': 'comunicazione e marketing',  # together with the previous one?
+
     #
     # ?? 'informatica: 'ingegneria informatica'
     'data science e intelligenza artificiale': 'data science e artificial intelligence',
     'data science': 'data science e artificial intelligence',
+    'scienze dei dati': 'data science e artificial intelligence',
+    #
+    "scienze della formazione primaria": "scienze della formazione",
 }
