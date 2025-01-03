2982606017e8b177d1c62157b421fe2b5d2f851e
Luca Benedetto <luca.benedetto93@gmail.com>
2025-01-03 09:11:40 +0000

    add arg system_message to prepare_input_text

--- src/huggingface_llm_recommender.py
+++ src/huggingface_llm_recommender.py
@@ -41,7 +41,7 @@ class HuggingFaceLLMRecommender(BaseLLMRecommender):
         return response
 
     # TODO
-    def prepare_input_text(self, user_prompt: str, system_message: Optional[str]) -> str:
+    def prepare_input_text(self, user_prompt: str, system_message: Optional[str] = None) -> str:
         raise NotImplementedError()
         # input_text = self.prompt
         # if self.model_name == LLAMA_3_8B:
