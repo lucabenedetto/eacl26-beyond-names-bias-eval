f0a20932e76856a575e2dad3546f77884b09d2a6
Luca Benedetto <luca.benedetto93@gmail.com>
2025-01-03 08:58:05 +0000

    add prepare_input_text to the base class

--- src/base_llm_recommender.py
+++ src/base_llm_recommender.py
@@ -5,3 +5,6 @@ class BaseLLMRecommender(object):
 
     def perform_recommendation(self, prompt: str, temperature: float, **kwargs) -> str:
         raise NotImplementedError("perform_recommendation not implemented for the Base class, use the other classes.")
+
+    def prepare_input_text(self, text: str) -> str:
+        raise NotImplementedError("prepare_input_text not implemented for the Base class, use the other classes.")
