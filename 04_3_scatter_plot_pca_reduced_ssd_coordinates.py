aad6cc85649f67139b804a8f16e540586758e46c
Luca Benedetto <luca.benedetto93@gmail.com>
2025-02-12 17:47:14 +0000

    add first version script to plot the scatter plot of the 2d reduced ssd coordinates (with marginals) and print recommendations from the borders and corners

---
+++ 04_3_scatter_plot_pca_reduced_ssd_coordinates.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+
+def get_pca_coordinates_by_study_group(df, study_group):
+    return np.array([(x1, x2) for x1, x2 in df[df['study_group'] == study_group][['pca_0', 'pca_1']].values])
+
+
+def scatter_plot_with_marginal_hist(
+        df_with_2d_coordinates, 
+        title='',
+) -> None:
+    """
+    Generate a scatter plot with marginal histograms for the given coordinates grouped by type.
+    """
+
+    coordinates = {
+        'model': get_pca_coordinates_by_study_group(df_with_2d_coordinates, 'model'),
+        'f': get_pca_coordinates_by_study_group(df_with_2d_coordinates, 'f'),
+        'm': get_pca_coordinates_by_study_group(df_with_2d_coordinates, 'm'),
+        'x': get_pca_coordinates_by_study_group(df_with_2d_coordinates, 'x'),
+    }
+
+    # this the code for a "standard" scatter plot.
+    # fig, ax = plt.subplots()
+    # ax.scatter(coordinates['model'][:,0], coordinates['model'][:,1], label='model', alpha=0.5)
+    # ax.scatter(coordinates['f'][:,0], coordinates['f'][:,1], label='f', alpha=0.5)
+    # ax.scatter(coordinates['m'][:,0], coordinates['m'][:,1], label='m', alpha=0.5)
+    # ax.scatter(coordinates['x'][:,0], coordinates['x'][:,1], label='x', alpha=0.5)
+    # ax.legend()
+    # plt.show()
+
+    fig = plt.figure(figsize=(10, 10))
+    gs = fig.add_gridspec(3, 3)
+    # Create the scatter plot
+    ax_scatter = fig.add_subplot(gs[1:, :-1])
+    for key, value in coordinates.items():
+        ax_scatter.scatter([x[0] for x in value], [x[1] for x in value], label=key, alpha=0.5)
+    ax_scatter.legend()
+    ax_scatter.grid(True)
+    # Create the x-axis histograms
+    ax_histx = fig.add_subplot(gs[0, :-1])
+    for key, value in coordinates.items():
+        ax_histx.hist([x[0] for x in value], bins=30, alpha=0.5)  # color=blue
+    ax_histx.set_xticks([])
+    # Create the y-axis histograms
+    ax_histy = fig.add_subplot(gs[1:, -1])
+    for key, value in coordinates.items():
+        ax_histy.hist([x[1] for x in value], bins=30, orientation='horizontal', alpha=0.5)  # color=blue
+    ax_histy.set_yticks([])
+
+    # Add a title
+    plt.suptitle(title)  # , y=0.95)
+    plt.tight_layout()
+    plt.show()
+
+
+def get_x_y_min_max(df_with_2d_coordinates):
+    x_min = df_with_2d_coordinates['pca_0'].min()
+    x_max = df_with_2d_coordinates['pca_0'].max()
+    y_min = df_with_2d_coordinates['pca_1'].min()
+    y_max = df_with_2d_coordinates['pca_1'].max()
+    return x_min, x_max, y_min, y_max
+
+
+def get_x_y_low_high_thresholds(df_with_2d_coordinates, n_bins):
+    x_min, x_max, y_min, y_max = get_x_y_min_max(df_with_2d_coordinates)
+    low_threshold_x = x_min + (x_max-x_min)/n_bins
+    high_threshold_x = x_max - (x_max-x_min)/n_bins
+    low_threshold_y = y_min + (y_max-y_min)/n_bins
+    high_threshold_y = y_max - (y_max-y_min)/n_bins
+    return low_threshold_x, high_threshold_x, low_threshold_y, high_threshold_y
+
+
+def print_recommendations_from_corners(df_2d_coord, n_bins=5):
+    # df_2d_coord must be a dataframe with at least cols pca_0, pca_1, and recs (I might actually change this so that
+    #   it works on different column names)
+    low_threshold_x, high_threshold_x, low_threshold_y, high_threshold_y = get_x_y_low_high_thresholds(df_2d_coord, n_bins)
+
+    bottom_left = df_2d_coord[(df_2d_coord['pca_0'] < low_threshold_x) & (df_2d_coord['pca_1'] < low_threshold_y)]
+    top_left = df_2d_coord[(df_2d_coord['pca_0'] < low_threshold_x) & (df_2d_coord['pca_1'] > high_threshold_y)]
+    top_right = df_2d_coord[(df_2d_coord['pca_0'] > high_threshold_x) & (df_2d_coord['pca_1'] > high_threshold_y)]
+    bottom_right = df_2d_coord[(df_2d_coord['pca_0'] > high_threshold_x) & (df_2d_coord['pca_1'] < low_threshold_y)]
+
+    print("Bottom left:", bottom_left['recs'].value_counts())
+    print("Top left:", top_left['recs'].value_counts())
+    print("Top Right:", top_right['recs'].value_counts())
+    print("Bottom right:", bottom_right['recs'].value_counts())
+
+
+def print_recommendations_from_borders(df_2d_coord, n_bins=10):
+    # df_2d_coord must be a dataframe with at least cols pca_0, pca_1, and recs (I might actually change this so that
+    #   it works on different column names)
+    low_threshold_x, high_threshold_x, low_threshold_y, high_threshold_y = get_x_y_low_high_thresholds(df_2d_coord, n_bins)
+    left = df_2d_coord[df_2d_coord['pca_0'] < low_threshold_x]
+    right = df_2d_coord[df_2d_coord['pca_0'] > high_threshold_x]
+    top = df_2d_coord[df_2d_coord['pca_1'] > high_threshold_y]
+    bottom = df_2d_coord[df_2d_coord['pca_1'] < low_threshold_y]
+    print("Left:", left['recs'].value_counts())
+    print("Right:", right['recs'].value_counts())
+    print("Top:", top['recs'].value_counts())
+    print("Bottom:", bottom['recs'].value_counts())
+
+
+def main():
+    df = pd.read_csv(os.path.join('data', 'processed_output', f'pca_reduced_ssd_coordinates_Anthropic.csv'))
+    scatter_plot_with_marginal_hist(df)
+    print_recommendations_from_corners(df)
+    print_recommendations_from_borders(df)
+
+
+if __name__ == '__main__':
+    main()
