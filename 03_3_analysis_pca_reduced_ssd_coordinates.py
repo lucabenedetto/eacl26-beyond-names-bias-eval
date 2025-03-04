ef9666f6d03371fb3c72212e5f44644a4ed23417
Luca Benedetto <luca.benedetto93@gmail.com>
2025-03-04 10:34:22 +0000

    add method to plot the EMD between the distribution of PCA1 and PCA2 in different study groups.

--- 03_3_analysis_pca_reduced_ssd_coordinates.py
+++ 03_3_analysis_pca_reduced_ssd_coordinates.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import pandas as pd
 import seaborn as sns
+from scipy.stats import wasserstein_distance
 
 from constants import (
     C_STUDY_GROUP, C_PCA_0, C_PCA_1, C_RECS,
@@ -186,6 +187,52 @@ def print_recommendations_from_borders(df_2d_coord, n_bins=10):
     print("Bottom:", bottom[C_RECS].value_counts())
 
 
+def confusion_matrix_distribution_distance(
+        df,
+        class_column,
+        x_column,
+        y_column,
+        # vmax,
+        title='hexbin by class',
+        output_file=None,
+):
+    # TODO: possibly change this so that it doesn't use the EMD by default but rather I can pass the method.
+
+    # TODO: make a method for this, it is used in several scripts/methods.
+    n_study_groups = df[class_column].nunique()
+    if len(STUDY_GROUPS) != n_study_groups:
+        raise ValueError("The number of study groups in the DF is not the expected one.")
+
+
+    fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+    for ax_idx, column in enumerate([x_column, y_column]):
+        conf_mat = np.zeros((n_study_groups, n_study_groups))
+
+        for (idx_1, study_group_1) in enumerate(STUDY_GROUPS):
+            stem_magnitudes_1 = df[df[class_column] == study_group_1][column].tolist()
+            for (idx_2, study_group_2) in enumerate(STUDY_GROUPS):
+                stem_magnitudes_2 = df[df[class_column] == study_group_2][column].tolist()
+                conf_mat[idx_1, idx_2] = wasserstein_distance(stem_magnitudes_1, stem_magnitudes_2)
+
+        print(f'Confusion matrix {column}:')
+        print(conf_mat)
+
+        im = ax[ax_idx].imshow(conf_mat, cmap='Reds')  # , vmax=vmax)  # TODO: Possibly readd vmax
+        # Show all ticks and label them with the respective list entries
+        ax[ax_idx].set_xticks(range(len(STUDY_GROUPS)), labels=STUDY_GROUPS, rotation=45, ha="right", rotation_mode="anchor")
+        ax[ax_idx].set_yticks(range(len(STUDY_GROUPS)), labels=STUDY_GROUPS)
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(STUDY_GROUPS)):
+            for j in range(len(STUDY_GROUPS)):
+                text = ax[ax_idx].text(j, i, "%.2f" % conf_mat[i, j], ha="center", va="center")
+        ax[ax_idx].set_title(f"EMD distance ({column}).")
+    fig.tight_layout()
+    if output_file:
+        plt.savefig(output_file)
+    else:
+        plt.show()
+
+
 def main():
     df = pd.read_csv(os.path.join('data', 'processed_output', f'pca_reduced_ssd_coordinates_aggregate.csv'))
 
@@ -205,6 +252,15 @@ def main():
     )
     # print_recommendations_from_corners(df, n_bins=5)
     # print_recommendations_from_borders(df, n_bins=15)
+
+    print("EMD between distribution of PCA reduced s.s.d. coordinates.")
+    confusion_matrix_distribution_distance(
+        df,
+        C_STUDY_GROUP,
+        C_PCA_0,
+        C_PCA_1,
+        # output_file=os.path.join(OUTPUT_FOLDER, f'{WHICH_PCA}__{WHICH_MODEL_AND_PARAMS}__conf_mat_EMD_ssd_coordinates.png'),
+        )
 
     print("scatter_plot_with_marginal_distributions_sns")
     scatter_plot_with_marginal_distributions_sns(
