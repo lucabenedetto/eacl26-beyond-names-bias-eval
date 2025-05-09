3e05424f6149c8ad8d6ad4b613b6f749d60f69a7
Luca Benedetto <luca.benedetto93@gmail.com>
2025-05-09 08:10:27 +0100

    now use constant for list SSD ids

--- 05_4_barplot_for_recs_v2.py
+++ 05_4_barplot_for_recs_v2.py
@@ -8,18 +8,18 @@ import seaborn as sns
 from constants import (
     C_STUDY_GROUP,
     MODELS_BY_OWNER,
+    C_LIST_SSD,
 )
 from course_mappings import LIST_SSD
 
 
 # TODO: Possibly move these to the constants or course mappings file.
-LIST_SSD_IDS = ['SSD_0','SSD_1','SSD_2','SSD_3','SSD_4','SSD_5','SSD_6','SSD_7','SSD_8','SSD_9','SSD_10','SSD_11','SSD_12','SSD_13']
-MAP_SSD_ID_TO_NAME = {ssd: LIST_SSD[idx] for idx, ssd in enumerate(LIST_SSD_IDS)}
+MAP_SSD_ID_TO_NAME = {ssd: LIST_SSD[idx] for idx, ssd in enumerate(C_LIST_SSD)}
 print(MAP_SSD_ID_TO_NAME)
 
 
 def compute_df_for_bar_plot_visualisation(df: pd.DataFrame):
-    df_recommended_ssd_count = df.groupby(C_STUDY_GROUP).sum()[LIST_SSD_IDS]
+    df_recommended_ssd_count = df.groupby(C_STUDY_GROUP).sum()[C_LIST_SSD]
     # this DF has one row for each study group, and one column for each SSD. The value of each cell is the frequency of the recommendation of the corresponding SSD (normalised)
     df_normalized = df_recommended_ssd_count.div(df_recommended_ssd_count.sum(axis=1), axis=0).reset_index()
     print(df_normalized)
