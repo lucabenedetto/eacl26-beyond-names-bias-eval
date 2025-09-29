d090be9112f59e782462b8ed3f318e8595bc8e80
Luca Benedetto <luca.benedetto93@gmail.com>
2025-09-29 15:45:49 +0200

    update to run new prompt config, create folder if needed, and remove deprecated analysis

--- 03_3_full_run_analysis_pca_reduced_ssd_coordinates.py
+++ 03_3_full_run_analysis_pca_reduced_ssd_coordinates.py
@@ -14,6 +14,7 @@ from constants import (
     MODELS_LIST,
     USER_AS_STUDENT, 
     LLM_AS_STUDENT,
+    THIRD_PERSON_AS_STUDENT,
     CONFIG_NO_NAME,
     CONFIG_W_NAMES,
 )
@@ -298,6 +299,14 @@ def run_analysis_pca_reduced_ssd_coordinates(df, output_folder, which_pca, which
 
 
 def run_complete_analysis_pca_reduced_ssd_coordinates(df, WHICH_PCA, OUTPUT_FOLDER):
+
+    # Create the folder if it doesn't exist
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+        print(f"Folder '{OUTPUT_FOLDER}' created.")
+    else:
+        print(f"Folder '{OUTPUT_FOLDER}' already exists.")
+
     # Analysis aggregating all the models and runs.
     run_analysis_pca_reduced_ssd_coordinates(df, OUTPUT_FOLDER, WHICH_PCA, 'aggregate')
 
@@ -310,9 +319,9 @@ def run_complete_analysis_pca_reduced_ssd_coordinates(df, WHICH_PCA, OUTPUT_FOLD
     for temperature in [0.0, 0.3, 0.6]:
         local_df = df[df['temperature'] == temperature]
         run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, f'aggregate_temp_{temperature}')
-    for temperatures in [[0.0, 0.3], [0.3, 0.6]]:
-        local_df = df[df['temperature'].isin(temperatures)]
-        run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, f'aggregate_temp_{temperatures[0]}_{temperatures[1]}')
+    # for temperatures in [[0.0, 0.3], [0.3, 0.6]]:
+    #     local_df = df[df['temperature'].isin(temperatures)]
+    #     run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, f'aggregate_temp_{temperatures[0]}_{temperatures[1]}')
 
     # analysis on different temperature values and different models
     for model_owner, list_models in MODELS_BY_OWNER.items():
@@ -327,13 +336,13 @@ def run_complete_analysis_pca_reduced_ssd_coordinates(df, WHICH_PCA, OUTPUT_FOLD
         run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, model)
 
     # analysis on the different prompt types
-    for prompt_type in [USER_AS_STUDENT, LLM_AS_STUDENT]:
+    for prompt_type in [USER_AS_STUDENT, LLM_AS_STUDENT, THIRD_PERSON_AS_STUDENT]:
         local_df = df[df['prompt_type'] == prompt_type]
         run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, f'aggregate_{prompt_type}')
 
     # analysis on the different prompt types and families of models
     for model_owner, list_models in MODELS_BY_OWNER.items():
-        for prompt_type in [USER_AS_STUDENT, LLM_AS_STUDENT]:
+        for prompt_type in [USER_AS_STUDENT, LLM_AS_STUDENT, THIRD_PERSON_AS_STUDENT]:
             local_df = df[df['model'].isin(list_models)]
             local_df = local_df[local_df['prompt_type'] == prompt_type]
             run_analysis_pca_reduced_ssd_coordinates(local_df, OUTPUT_FOLDER, WHICH_PCA, f'{model_owner}_{prompt_type}')
@@ -343,7 +352,7 @@ if __name__ == '__main__':
     df = pd.read_csv(os.path.join('data', 'processed_output', f'pca_reduced_ssd_coordinates_aggregate.csv'))
 
     WHICH_PCA = 'agg_pca'  # The PCA model to use. All results in the paper are the ones obtained using the aggragete model (trained on all provided recommendations).
-    RUN_DATE = "2025_05_for_paper"
+    RUN_DATE = "2025_09_29_for_paper"
 
     print("Doing both with and without names")
     OUTPUT_FOLDER = os.path.join('figures', RUN_DATE, 'analysis_pca_reduced_ssd_aggregate')
